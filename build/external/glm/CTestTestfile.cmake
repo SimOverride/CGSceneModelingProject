27d144d92080659e61367508d157f03cc897d1c7
Sim <71499535+SimOverride@users.noreply.github.com>
2025-04-18 20:00:33 +0800

    Delete build directory

--- build/external/glm/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Workbench_Local/CG/CGSceneModelingProject/external/glm
-# Build directory: D:/Workbench_Local/CG/CGSceneModelingProject/build/external/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
--- build/external/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Workbench_Local/CG/CGSceneModelingProject/external/glm/glm
-# Build directory: D:/Workbench_Local/CG/CGSceneModelingProject/build/external/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
